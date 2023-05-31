cfafca3416ca0db1cb378f3802de18513656c9ce
Daisu27 <65787542+Daisu27@users.noreply.github.com>
2023-05-31 16:32:22 +0800

    Delete redundant space in CWE-73.py

--- CWE-73/CWE-73.py
+++ CWE-73/CWE-73.py
@@ -1,35 +1,25 @@
 from quark.script import runQuarkAnalysis, Rule
 
-
 SAMPLE_PATH = "ovaa.apk"
 RULE_PATH = "accessFileInExternalDir.json"
 
 OPEN_FILE_API = [
-    "Landroid/os/ParcelFileDescriptor;",                     # Class name
-    "open",                                                  # Method name   
-    "(Ljava/io/File; I)Landroid/os/ParcelFileDescriptor;"    # Descriptor
+    "Landroid/os/ParcelFileDescriptor;",                   # Class name
+    "open",                                                # Method name
+    "(Ljava/io/File; I)Landroid/os/ParcelFileDescriptor;"  # Descriptor
 ]
 
-rule_instance = Rule(RULE_PATH)
-quark_result = runQuarkAnalysis(SAMPLE_PATH, rule_instance)
+ruleInstance = Rule(RULE_PATH)
+quarkResult = runQuarkAnalysis(SAMPLE_PATH, ruleInstance)
 
-for access_external_dir in quark_result.behavior_occur_list:
-    file_path = access_external_dir.second_api.get_arguments()[2]
+for accessExternalDir in quarkResult.behaviorOccurList:
+    filePath = accessExternalDir.secondAPI.getArguments()[2]
 
-    if quark_result.is_hardcoded(file_path):
+    if quarkResult.isHardcoded(filePath):
         continue
 
-    caller = access_external_dir.method_caller
-    result = quark_result.find_method_in_caller(caller, OPEN_FILE_API)
+    caller = accessExternalDir.methodCaller
+    result = quarkResult.findMethodInCaller(caller, OPEN_FILE_API)
 
     if result:
-        print("CWE-73 is detected in method:", caller.full_name)
-
-
-
-
-
-
-    
-
-    
+        print("CWE-73 is detected in method, ", caller.fullName)
