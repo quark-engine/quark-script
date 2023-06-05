c849b948a469f7fbfacd4f30dd5713cb31cfdddd
LiangPPP <61388755+LiangPPP@users.noreply.github.com>
2023-06-06 01:30:51 +0800

    Update CWE-88.py

--- CWE-88/CWE-88.py
+++ CWE-88/CWE-88.py
@@ -1,32 +1,32 @@
-from quark.script import runQuarkAnalysis, Rule
+from quark.script import runQuarkAnalysis, Rule, findMethodInAPK
 
 SAMPLE_PATH = "Vuldroid.apk"
 RULE_PATH = "ExternalStringCommand.json"
 
 
-STRING_MATCHING_API = [
-    ["Ljava/lang/String;", "contains", "(Ljava/lang/CharSequence)Z"],
-    ["Ljava/lang/String;", "indexOf", "(I)I"],
-    ["Ljava/lang/String;", "indexOf", "(Ljava/lang/String;)I"],
-    ["Ljava/lang/String;", "matches", "(Ljava/lang/String;)Z"],
-    ["Ljava/lang/String;", "replaceAll",
-        "(Ljava/lang/String; Ljava/lang/String;)Ljava/lang/String;"],
-]
+STRING_MATCHING_API = set([
+    ("Ljava/lang/String;", "contains", "(Ljava/lang/CharSequence)Z"),
+    ("Ljava/lang/String;", "indexOf", "(I)I"),
+    ("Ljava/lang/String;", "indexOf", "(Ljava/lang/String;)I"),
+    ("Ljava/lang/String;", "matches", "(Ljava/lang/String;)Z"),
+    ("Ljava/lang/String;", "replaceAll", "(Ljava/lang/String; Ljava/lang/String;)Ljava/lang/String;")
+])
 
-delimiters = [' ', ';', '||', '|', ',', '>', '>>', '`']
+delimeter = "-"
 
 ruleInstance = Rule(RULE_PATH)
 quarkResult = runQuarkAnalysis(SAMPLE_PATH, ruleInstance)
 
 for ExternalStringCommand in quarkResult.behaviorOccurList:
 
+    methodCalled = set()
     caller = ExternalStringCommand.methodCaller
 
-    strMatchingAPIs = [
-        api for api in STRING_MATCHING_API if
-        quarkResult.findMethodInCaller(caller, api)
-    ]
+    for method in ExternalStringCommand.getMethodsInArgs():
+        methodCalled.add(method.fullName)
 
-    if not strMatchingAPIs or \
-            any(dlm not in strMatchingAPIs for dlm in delimiters):
+    if methodCalled.intersection(STRING_MATCHING_API):
+        if not ExternalStringCommand.hasString(delimeter):
+            print(f"CWE-88 is detected in method, {caller.fullName}")
+    else:
         print(f"CWE-88 is detected in method, {caller.fullName}")
