88c97682caad57a8f33dd8451b6732cee731b890
AB2 <84577967+ab2pentest@users.noreply.github.com>
2023-07-05 06:36:08 +0200

    Fixing IndexError on CWE-749.py

--- CWE-749/CWE-749.py
+++ CWE-749/CWE-749.py
@@ -17,8 +17,11 @@ for configureJsExecution in quarkResult.behaviorOccurList:
     caller = configureJsExecution.methodCaller
     secondAPI = configureJsExecution.secondAPI
 
-    enableJS = secondAPI.getArguments()[1]
-    exposeAPI = quarkResult.findMethodInCaller(caller, targetMethod)
+    # Check if arguments list has at least two elements
+    arguments = secondAPI.getArguments()
+    if len(arguments) >= 2:
+        enableJS = arguments[1]
+        exposeAPI = quarkResult.findMethodInCaller(caller, targetMethod)
 
-    if enableJS and exposeAPI:
-        print(f"CWE-749 is detected in method, {caller.fullName}")
+        if enableJS and exposeAPI:
+            print(f"CWE-749 is detected in method, {caller.fullName}")
