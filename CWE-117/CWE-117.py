64556e06636813e414e7041d4b7c9059961f8ae2
LiangPPP <61388755+LiangPPP@users.noreply.github.com>
2023-11-16 19:48:14 +0800

    Create CWE-117.py

---
+++ CWE-117/CWE-117.py
@@ -0,0 +1,21 @@
+from quark.script import Rule, runQuarkAnalysis
+
+SAMPLE_PATH = "allsafe.apk"
+RULE_PATH = "writeContentToLog.json"
+KEYWORDS_FOR_NEUTRALIZATION = ["escape", "replace", "format", "setFilter"]
+
+ruleInstance = Rule(RULE_PATH)
+quarkResult = runQuarkAnalysis(SAMPLE_PATH, ruleInstance)
+
+for logOutputBehavior in quarkResult.behaviorOccurList:
+
+    secondAPIParam = logOutputBehavior.getParamValues()[1]
+
+    isKeywordFound = False
+    for keyword in KEYWORDS_FOR_NEUTRALIZATION:
+        if keyword in secondAPIParam:
+            isKeywordFound = True
+            break
+
+    if not isKeywordFound:
+        print(f"CWE-117 is detected in method,{secondAPIParam}")
