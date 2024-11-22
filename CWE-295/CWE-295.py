aac08fe556c79be38671c846678ef1f4b4c4f6a5
ZhiH <127014343+JerryTasi@users.noreply.github.com>
2024-11-22 17:52:34 +0800

    Optimize the showcase of Quark Script CWE-295

--- CWE-295/CWE-295.py
+++ CWE-295/CWE-295.py
@@ -6,15 +6,17 @@ TARGET_METHOD = [
     "proceed",                           # method name
     "()V"                                # descriptor
 ]
-OVERRIDE_METHOD = [
+OVERRIDDEN_METHOD = [
     "Landroid/webkit/WebViewClient;",    # class name
     "onReceivedSslError",                # method name
-    "(Landroid/webkit/WebView;"+" Landroid/webkit/SslErrorHandler;" + \
+    "(Landroid/webkit/WebView;" + " Landroid/webkit/SslErrorHandler;" + \
     " Landroid/net/http/SslError;)V"     # descriptor
 ]
 
 for sslProceedCaller in findMethodInAPK(SAMPLE_PATH, TARGET_METHOD):
-    if (sslProceedCaller.name == OVERRIDE_METHOD[1] and
-            sslProceedCaller.descriptor == OVERRIDE_METHOD[2] and
-            OVERRIDE_METHOD[0] in sslProceedCaller.findSuperclassHierarchy()):
+    if (
+        sslProceedCaller.name == OVERRIDDEN_METHOD[1]
+        and sslProceedCaller.descriptor == OVERRIDDEN_METHOD[2]
+        and OVERRIDDEN_METHOD[0] in sslProceedCaller.findSuperclassHierarchy()
+    ):
         print(f"CWE-295 is detected in method, {sslProceedCaller.fullName}")
